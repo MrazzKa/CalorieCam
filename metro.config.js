652f1346ee55febf13fe5899cad3b358f6e1975d
Timur <your_email@example.com>
2025-11-11 15:51:41 +0500

    New 11.11.25

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,16 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
+
+config.resolver = {
+  ...config.resolver,
+  blockList: exclusionList([
+    /apps\/api\/.*/,
+    /.*\.(mp4|mov|mkv|zip|psd|7z|rar)$/i,
+    /logs\/.*/,
+  ]),
+};
 
 // Configure Metro to use specific host if needed
 // This helps when running in WSL2 with hotspot
